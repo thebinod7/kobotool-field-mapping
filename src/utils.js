a90b79387c415b49e20af8dbdbfb429d04e5d610
Binod Chaudhary <thebinod7@gmail.com>
2024-01-26 10:18:08 +0545

    cleanup

--- src/utils.js
+++ src/utils.js
@@ -41,18 +41,6 @@ export const includeOnlySelectedTarget = (array, selectedTargets) => {
 	});
 };
 
-// export const attachedRawData = (payload, rawDataSource) => {
-// 	let result = [];
-// 	for (let p of payload) {
-// 		const found = rawDataSource.find((f) => f._id === p._id);
-// 		if (found) {
-// 			const newItem = { ...p, rawData: found };
-// 			result.push(newItem);
-// 		}
-// 	}
-// 	return result;
-// };
-
 export const attachedRawData = (payload, rawDataSource) => {
 	let result = [];
 	for (let i = 0; i < payload.length; i++) {
