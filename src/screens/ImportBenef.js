addb2f5c4bc907593904891d46ecfbd3a547d311
RS21 <rs21@pop-os.localdomain>
2024-02-19 15:37:50 +0545

    fix(core):added authorization in app for temp purpose

--- src/screens/ImportBenef.js
+++ src/screens/ImportBenef.js
@@ -3,76 +3,85 @@ import React, { useEffect, useState } from "react";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-function ImportBenef({ handleHomeClick }) {
-	const [sources, setSources] = useState([]);
+function ImportBenef({ handleHomeClick, authToken }) {
+  const [sources, setSources] = useState([]);
 
-	const handleImortClick = async (uuid) => {
-		try {
-			const res = await axios.get(
-				`${API_URL}/beneficiary-imports/${uuid}/import`
-			);
-			alert(res.data.data.message);
-		} catch (err) {
-			alert(err.response.data.message);
-		}
-	};
+  const handleImortClick = async (uuid) => {
+    try {
+      const res = await axios.get(
+        `${API_URL}/beneficiary-imports/${uuid}/import`,
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      alert(res.data.data.message);
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+  };
 
-	useEffect(() => {
-		async function fetchSources() {
-			const res = await axios.get(`${API_URL}/sources`);
-			const { data } = res;
-			setSources(data.data.rows);
-		}
+  useEffect(() => {
+    async function fetchSources() {
+      const res = await axios.get(`${API_URL}/sources`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+      const { data } = res;
+      setSources(data.data.rows);
+    }
 
-		fetchSources();
-	}, []);
+    fetchSources();
+  }, []);
 
-	return (
-		<div style={{ margin: 20, padding: 20 }}>
-			<h3>Beneficiary Source List</h3>
-			<a onClick={(e) => handleHomeClick()} href="#home">
-				[Go Home]
-			</a>
-			<table>
-				<thead>
-					<th>Source</th>
-					<th>Created At</th>
-					<th>Beneficiary Count</th>
-					<th>Action</th>
-				</thead>
+  return (
+    <div style={{ margin: 20, padding: 20 }}>
+      <h3>Beneficiary Source List</h3>
+      <a onClick={(e) => handleHomeClick()} href="#home">
+        [Go Home]
+      </a>
+      <table>
+        <thead>
+          <th>Source</th>
+          <th>Created At</th>
+          <th>Beneficiary Count</th>
+          <th>Action</th>
+        </thead>
 
-				<tbody>
-					{!!sources.length ? (
-						sources.map((d) => {
-							return (
-								<tr key={d.id}>
-									<td>{d.name}</td>
-									<td>{d.created_at}</td>
-									<td>{d.fieldMapping.data.length}</td>
-									<td>
-										{d.isImported ? (
-											"Imported!"
-										) : (
-											<button
-												type="button"
-												onClick={() => handleImortClick(d.uuid)}
-											>
-												Import Beneficiary
-											</button>
-										)}
-									</td>
-								</tr>
-							);
-						})
-					) : (
-						<tr>
-							<td>No data found</td>
-						</tr>
-					)}
-				</tbody>
-			</table>
-		</div>
-	);
+        <tbody>
+          {!!sources.length ? (
+            sources.map((d) => {
+              return (
+                <tr key={d.id}>
+                  <td>{d.name}</td>
+                  <td>{d.created_at}</td>
+                  <td>{d.fieldMapping.data.length}</td>
+                  <td>
+                    {d.isImported ? (
+                      "Imported!"
+                    ) : (
+                      <button
+                        type="button"
+                        onClick={() => handleImortClick(d.uuid)}
+                      >
+                        Import Beneficiary
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td>No data found</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default ImportBenef;
